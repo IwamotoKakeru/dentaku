91349bf3648ab313894725627d4e2bc23efbe523
Kakeru <34148721+IwamotoKakeru@users.noreply.github.com>
2021-05-27 15:58:26 +0900

    Add files via upload
    ACの追加

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ def calc():
     ans.set(eval(form))
     print(ans)
 
+'''(miyagawa)
+def ac():
+    formEntry.delete(0, END)
+    ans.set("")
+'''
+
 #window設定
 window = Tk()
 window.title("calculator")
+
+'''(miyagawa)
+window.configure(background="#FFFFFF")
+window.geometry("200x100+200+200")
+'''
 
 ans = StringVar()
 
@@ -17,9 +28,17 @@ formEntry=Entry()
 eqalButton=Button(text="=",command=calc)
 ansLabel=Label(foreground='#000000', background='#FFFFFF',textvariable=ans)
 
+'''(miyagawa)
+ACbutton=Button(text="AC",command=ac)
+'''
+
 #widget配置
 formEntry.grid(row=0,column=0)
 eqalButton.grid(row=1,column=0)
 ansLabel.grid(row=1,column=1)
+
+'''(miyagawa)
+ACbutton.grid(row=1,column=3)
+'''
 
 window.mainloop()
